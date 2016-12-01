e878da1bb07ef3f59f37852967229a6681b2c455
Collin Kleiboer <collin@gissense.com>
2016-12-01 22:26:09 +0100

    files with licenses

--- ShowJSON/JsonHelper.cs
+++ ShowJSON/JsonHelper.cs
@@ -1,4 +1,21 @@
-﻿using ArcGIS.Core.CIM;
+﻿/*
+    Copyright GisSense 2016
+    This file is part of ShowJSON.
+
+    ShowJSON is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    ShowJSON is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with ShowJSON.If not, see<http://www.gnu.org/licenses/>.
+*/
+using ArcGIS.Core.CIM;
 using ArcGIS.Core.Geometry;
 using ArcGIS.Desktop.Framework.Threading.Tasks;
 using ArcGIS.Desktop.Mapping;
